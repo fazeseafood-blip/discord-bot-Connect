2a90f46c7981bb76fe1c96ca0f0349a358ba4737
fazeseafood-blip <fazeseafood@gmail.com>
2026-02-08 17:53:47 -0500

    fixing token login

--- src/index.js
+++ src/index.js
@@ -1655,4 +1655,8 @@ async function promptNewTicket(message, guild) {
   }
 }
 
-client.login(process.env.TOKEN);
+console.log('About to login with token...');
+client.login(process.env.TOKEN).catch(err => {
+  console.error('Failed to login:', err);
+  process.exit(1);
+});
